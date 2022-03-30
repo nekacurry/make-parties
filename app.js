4e2bea8426c968844c7953544ebc9f165f9101c4
nekacurry <anneka498@gmail.com>
2022-03-30 14:58:42 -0500

    proper separation of concerns

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const bodyParser = require('body-parser');
 const models = require('./db/models');
+require('./controllers/events')(app, models);
+
 
 app.use(methodOverride('_method'))
 
@@ -17,19 +19,6 @@ app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allo
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
-// OUR MOCK ARRAY OF PROJECTS
-var events = [
-    { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://cdn2.photostockeditor.com/t/2812/animal-raccoon-walking-on-lawn-grass-raccoon-raccoon-image.jpg" },
-    { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://cdn2.photostockeditor.com/t/2812/animal-raccoon-walking-on-lawn-grass-raccoon-raccoon-image.jpg" },
-    { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://cdn2.photostockeditor.com/t/2812/animal-raccoon-walking-on-lawn-grass-raccoon-raccoon-image.jpg" }
-  ]
-
-// Index
-app.get('/', (req, res) => {
-  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
-    res.render('events-index', { events: events });
-  })
-})
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
@@ -37,61 +26,4 @@ const port = process.env.PORT || 3000;
 // Tell the app what port to listen on
 app.listen(port, () => {
   console.log('App listening on port 3000!')
-})
-
-app.get('/events/new', (req, res) => {
-  res.render('events-new', {});
-})
-
-// CREATE
-app.post('/events', (req, res) => {
-  models.Event.create(req.body).then(event => {
-    res.redirect(`/events/${event.id}`);
-  }).catch((err) => {
-    console.log(err)
-  });
-})
-
-// SHOW
-app.get('/events/:id', (req, res) => {
-  // Search for the event by its id that was passed in via req.params
-  models.Event.findByPk(req.params.id).then((event) => {
-    // If the id is for a valid event, show it
-    res.render('events-show', { event: event })
-  }).catch((err) => {
-    // if they id was for an event not in our db, log an error
-    console.log(err.message);
-  })
-})
-
-// EDIT
-app.get('/events/:id/edit', (req, res) => {
-  models.Event.findByPk(req.params.id).then((event) => {
-    res.render('events-edit', { event: event });
-  }).catch((err) => {
-    console.log(err.message);
-  })
-});
-
-// UPDATE
-app.put('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then(event => {
-    event.update(req.body).then(event => {
-      res.redirect(`/events/${req.params.id}`);
-    }).catch((err) => {
-      console.log(err);
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-});
-
-// DELETE
-app.delete('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then(event => {
-    event.destroy();
-    res.redirect(`/`);
-  }).catch((err) => {
-    console.log(err);
-  });
 })
