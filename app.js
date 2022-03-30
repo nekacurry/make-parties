15a003bc3b3534b9237328bf49fb637a4fb5e193
nekacurry <anneka498@gmail.com>
2022-03-30 14:39:27 -0500

    Users can edit and update events

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // Initialize express
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const bodyParser = require('body-parser');
 const models = require('./db/models');
+
+app.use(methodOverride('_method'))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -60,3 +63,25 @@ app.get('/events/:id', (req, res) => {
     console.log(err.message);
   })
 })
+
+// EDIT
+app.get('/events/:id/edit', (req, res) => {
+  models.Event.findByPk(req.params.id).then((event) => {
+    res.render('events-edit', { event: event });
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+
+// UPDATE
+app.put('/events/:id', (req, res) => {
+  models.Event.findByPk(req.params.id).then(event => {
+    event.update(req.body).then(event => {
+      res.redirect(`/events/${req.params.id}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+});
